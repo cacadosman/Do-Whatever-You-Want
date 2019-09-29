0065b295eccb9bcc9203791f33f7565ea81da2b7
Fadli Maulana <wetmanz23@gmail.com>
2019-09-29 22:11:11 +0700

    Update cj_compiler.py

--- CJLang (esolang)/cj_compiler.py
+++ CJLang (esolang)/cj_compiler.py
@@ -1,6 +1,7 @@
-# Usage: python cj_compiler.py hello_world.cj
+# Usage: python cj_compiler.py <filename>
 
 import sys
+from functools import reduce
 
 opcode = {
     'ah': 0,
@@ -10,21 +11,31 @@ opcode = {
     'go': 4,
     'again': 5
 }
+dec_ops = []
 
 filename = sys.argv[1]
 
 f = open(filename, 'r')
-r = f.read().strip()
-data = r.split('cj')
-dec_ops = []
-for codes in data:
-    codes = list(filter(lambda c: c != '', codes.split(' ')))
-    codes_val = ''.join(list(map(lambda c: str(opcode[c]), codes)))
-    rev_codes_val = codes_val[::-1]
-    result = 0
-    for i in range(len(rev_codes_val)):
-        result += int(rev_codes_val[i]) * 6 ** i
-    dec_ops += [result]
+data = f.read().strip().split('cj')
 
-output = ''.join(list(map(lambda c: chr(c), dec_ops)))
-print(output)
+for codes in data:
+    codes = list(
+        filter(lambda c: c != '', codes.split(' '))
+    )
+    codes_val = ''.join(list(
+        map(lambda c: str(opcode[c]), codes)
+    ))
+    rev_codes_val = codes_val[::-1]
+    do_sum = lambda x,y: x + y
+
+    dec_ops += [
+        reduce(
+            do_sum, [
+                int(rev_codes_val[i]) * 6 ** i 
+                for i in range(len(rev_codes_val))
+            ]
+        )
+    ]
+
+out = ''.join(list(map(lambda c: chr(c), dec_ops)))
+print(out)
